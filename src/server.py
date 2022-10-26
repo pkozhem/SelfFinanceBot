f8d4e63255435456f820bd6b47145379f1ff1bfb
pkozhem <pkozhem@gmail.com>
2022-10-26 03:35:04 +0300

    Wrote message handlers and configured them.

--- src/server.py
+++ src/server.py
@@ -2,9 +2,11 @@
 
 import logging
 from aiogram import Bot, Dispatcher, types
+
 from src import settings
 from src.middlewares import AccessMiddleware
 from src.categories import Categories
+from src import expenses
 
 
 logging.basicConfig(level=logging.INFO)
@@ -25,7 +27,7 @@ async def start_command(message: types.Message) -> None:
         "Today expenses: /today\n"
         "Last month expenses: /month\n"
         "Last 5 expenses: /last\n"
-        "Delete expense: /delete"
+        "Delete expense: /delete{id}"
     )
 
 
@@ -40,36 +42,64 @@ async def show_categories(message: types.Message) -> None:
     await message.answer(answer)
 
 
-@dp.message_handler(commands=["today"])
+@dp.message_handler(commands="today")
 async def today_expenses(message: types.Message) -> None:
     """ Shows expenses summary for today. """
 
-    pass
+    answer = expenses.today_expenses()
+    await message.answer(answer)
 
 
-@dp.message_handler(commands=["month"])
+@dp.message_handler(commands="month")
 async def month_expenses(message: types.Message) -> None:
     """ Shows expenses summary for this month. """
 
-    pass
+    answer = expenses.month_expenses()
+    await message.answer(answer)
 
 
-@dp.message_handler(commands=["last"])
+@dp.message_handler(commands="last")
 async def last_expenses(message: types.Message) -> None:
     """ Shows last 5 expenses. """
 
-    pass
+    last = expenses.last_expenses()
+
+    if not last:
+        await message.answer("No expenses yet")
+        return
+
+    last_rows = [
+        f"{expense.price} on {expense.expense_category} — press "
+        f"/delete{expense.id_expense} to delete"
+        for expense in last]
+
+    answer = "{}{}".format(
+        "Latest expenses:\n\n*",
+        "\n*".join(last_rows)
+    )
+
+    await message.answer(answer)
 
 
-@dp.message_handler(lambda message: message.text.startwith("/delete"))
+@dp.message_handler(lambda message: message.text.startswith('/delete'))
 async def delete_expenses(message: types.Message) -> None:
     """ Deletes chosen expense. """
-
-    pass
+    pk = int(message.text[7:])
+    expenses.delete_expense(pk)
+    await message.answer("Expense deleted")
 
 
 @dp.message_handler()
 async def add_expenses(message: types.Message) -> None:
     """ Function which adds expense. """
 
-    await message.answer("timely placeholder")
+    try:
+        expense = expenses.add_expense(message.text)
+    except Exception as E:
+        await message.answer(str(E))
+        return
+
+    answer = (
+        f"Added expenses: {expense.price} on {expense.expense_category}.\n\n"
+        f"{expenses.today_expenses()}")
+    await message.answer(answer)
