c50433c6c1120b89f65b93a4af3751d94cd429a1
pkozhem <pkozhem@gmail.com>
2022-10-26 03:39:45 +0300

    Removed not necessary function.

--- src/manage.py
+++ src/manage.py
@@ -5,12 +5,6 @@ from aiogram import executor
 from src.db.core import database_state
 
 
-def dispatcher_register() -> None:
-    src.server.dp.register_message_handler(src.server.start_command, commands=["start", "help"])
-    src.server.dp.register_message_handler(src.server.show_categories, commands="categories")
-
-
 if __name__ == "__main__":
     database_state()
-    dispatcher_register()
     executor.start_polling(src.server.dp, skip_updates=True)
